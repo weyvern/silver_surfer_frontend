71f142ab84879dd295e4480920fdd9ce17db0227
Jorge Paul <jserranopaul@gmail.com>
2020-09-09 20:08:26 +0200

    Deploy

--- src/components/people/PeopleList.js
+++ src/components/people/PeopleList.js
@@ -1,22 +1,19 @@
 import React from 'react';
+import { Spinner } from 'react-rainbow-components';
 import PeopleItem from './PeopleItem';
 
 const PeopleList = ({ peopleList, type }) => {
 	return (
-<div className="d-flex">
-{peopleList ? (
-			peopleList.map(p => <PeopleItem key={p.user_id} person={p} type={type} />)
-		) : (
-			<Spinner />
-		)}
-</div>
-
-		
-	)
-	
-	
-	
-	
+		<div className="d-flex">
+			{peopleList ? (
+				peopleList.map(p => (
+					<PeopleItem key={p.user_id} person={p} type={type} />
+				))
+			) : (
+				<Spinner />
+			)}
+		</div>
+	);
 };
 
 export default PeopleList;
